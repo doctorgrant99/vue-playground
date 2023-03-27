f9ed63846243c6dfced9cdc634f3bff4e94ece64
Chandler Bowman <doctorgrant99@gmail.com>
2023-03-27 15:51:24 -0600

    vue practice

--- src/components/ConditionalRenderingExercise.vue
+++ src/components/ConditionalRenderingExercise.vue
@@ -10,7 +10,7 @@
             screen.
           </p>
           <!-- add code to element below -->
-          <div class="player-card text-center">
+          <div class="state.player-card text-center" v-if="false == false">
             <div>
               <img :src="state.player.photo" />
             </div>
@@ -38,7 +38,7 @@
             toggle the value to true to get it to show up again.
           </p>
           <!-- add code to element below -->
-          <div class="player-card text-center">
+          <div class="player-card text-center" v-if="state.showPlayer">
             <div>
               <img :src="state.player.photo" />
             </div>
@@ -129,6 +129,7 @@ export default {
   setup() {
     // NOTE typically state will be abstracted to a global AppState
     const state = reactive({
+      showPlayer: true,
       //add a property to toggle the player-card here.
       //add a property to set the number value here.
       player: {
